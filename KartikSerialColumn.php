2683f0dad77e815414b514408809a9c11b7f8f7c
mydream <381296986@qq.com>
2015-08-27 10:04:56 +0800

    use SerialColumnTrait and extends kartik
    use SerialColumnTrait and extends kartik

---
+++ KartikSerialColumn.php
@@ -0,0 +1,12 @@
+<?php
+namespace liyunfang\contextmenu;
+
+/**
+ * Description of kartikSerialColumn
+ * ContextMenu \kartik\grid\SerialColumn
+ * @author liyunfang <381296986@qq.com>
+ * @date 2015-08-27
+ */
+class KartikSerialColumn extends \kartik\grid\SerialColumn {
+    use SerialColumnTrait;
+}
